15e19229a784e8d0eaee6c1d8404da9213c2af1c
github username <github email address>
2020-05-13 11:31:27 -0400

    robot moves

--- index.js
+++ index.js
@@ -1,6 +1,83 @@
+
+
 document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+	const movesContainer = document.getElementById('moves-container');
+	
+	const validateMove = keyCode => {
+		if (keyCode === "ArrowUp" || 
+			keyCode === "ArrowDown" || 
+			keyCode === "ArrowLeft" || 
+			keyCode === "ArrowRight")
+			{
+				return true;
+			} else {
+				return false;
+			};
+	};
+
+	const isDelete = keyCode => {
+			if (keyCode === "Backspace" || keyCode === "Delete")
+			{
+				return true;
+			} else {
+				return false;
+			}
+	}
+
+	document.addEventListener('keydown', event => {
+		const keyCode = event.code;
+		if (validateMove(keyCode))
+			{
+				logMove(keyCode);
+			}
+		else if (isDelete(keyCode))
+			{
+				deleteLast();
+			}
+		else {
+			console.log('Use the arrow keys to log a move');
+			console.log('Use backpace or delete to remove last move')
+		}
+	});
+
+	const logMove = move => {
+		const newMove = document.createElement('li');
+		movesContainer.appendChild(newMove);
+		newMove.className = 'move-list-item';
+		newMove.innerHTML = getMoveDirection(move);
+	};
+
+	const getMoveDirection = keyCode => {
+		switch(keyCode){
+		case "ArrowUp":
+				return 'up';
+		case "ArrowDown":
+				return 'down';
+		case "ArrowLeft":
+				return 'left';
+		case "ArrowRight":
+				return 'right';
+		};
+	};
+
+	// Make the moves
+	const moveButton = document.getElementById("move-button");
+
+	const makeMoves = movesList => {
+		let movesArray = Array.from(movesContainer.children);
+		movesArray.forEach(moveRobot => {
+			move(moveRobot.innerHTML);
+			movesContainer.firstChild.remove();
+		})
+	};
+
+	// Delete Move
+	const deleteLast = () => {
+		movesContainer.lastChild.remove();
+	}
+
+	moveButton.addEventListener('click', makeMoves);
 })
